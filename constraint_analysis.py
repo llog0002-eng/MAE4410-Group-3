89edd1394b197a09ab812a5aacedf09eb119e321
llog0002-eng <llog0002@student.monash.edu>
2025-08-25 12:49:41 +1000

    test1

---
+++ constraint_analysis.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+### INPUTS
+rho = 1.225 # Air density, kg/m3
+betaw = 0.8 # Ratio of actual weight to maximum weight
+
+# AERO DEPENDENT
+CD0 = 0.02 # Zero lift drag
+K = 0.06 # Induced drag constant
+
+# ENGINE DEPENDENT
+alphae = 69.2/375.3 # Ratio of maximum static thrust or power at sea level to the thrust or power at the desired operating condition
+# source: https://www.kimerius.com/app/download/5781574315/The+GE90+-+An+introduction.pdf
+
+# Generate x and y arrays
+
+### Maximum speed requirement
+
+
+### T/O distance requirement
+
+
+### Climb rate requirement
+
+
+### Service ceiling requirement
+
+
+### Stall speed requirement
+
+
